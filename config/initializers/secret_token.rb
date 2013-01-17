189956e6110ffa4268b8c1f4b3c219f78370ecfe
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-01-17 16:21:21 +0900

    [2.1] first commit
     * generate demo_app
     * modify Gemfile
     * execute bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1cf738d7ca10e10d48253db35b13cccc672216bea2e052e8cd45824dc89290c2567e763a8e38605768a9e92794ceccc7d4182949af476572eb659106072f450b'
